838f8061b0fa40024c8f20280fab92f8e4c1920d
Owen <owentscooper@gmail.com>
2020-11-25 18:32:53 -0500

    jamies vision code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/Vuforia.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.Vision;
+
+import android.graphics.Bitmap;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.internal.android.dx.util.Warning;
+
+public class Vuforia {
+    private VuforiaLocalizer vuforiaLocalizer;
+
+
+    public Vuforia() {
+        //int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId"," id", Utils.hardwareMap.appContext.getPackageName());
+        //VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters();
+        params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        params.vuforiaLicenseKey = "AbuxcJX/////AAABmXadAYnA80uwmb4Rhy4YmvIh7qg/f2yrRu1Nd8O7sSufbUWHSv1jDhunwDBItvFchrvkc8EjTzjh97m2kAPy8YOjBclQbEBtuR8qcIfrGofASCZh2M6vQ0/Au+YbhYh0MLLdNrond+3YjkLswv6+Se3eVGw9y9fPGamiABzIrosjUdanAOWemf8BtuQUW7EqXa4mNPtQ+2jpZQO2sqtqxGu1anHQCD0S/PvdZdB7dRkyWaH6XTZCat5gZ0fpFH/aLWMFP4yiknlgYbjT7gklUAqyDX81pNrQhWWY4dOFnz2WiWhkCt+MNZMLKH5SdsyC7gwKI/r3h51pTwgXZfyYymB60eYAFqEUpeTrL+4LmltN";
+
+        vuforiaLocalizer = ClassFactory.getInstance().createVuforia(params);
+        vuforiaLocalizer.enableConvertFrameToBitmap();
+        vuforiaLocalizer.setFrameQueueCapacity(1);
+    }
+
+    /**
+     * Grabs a bitmap from the Vuforia engine for image processing
+     * @return camera output as a bitmap
+     */
+    public Bitmap getBitmap(){
+        Bitmap bitmap;
+        VuforiaLocalizer.CloseableFrame frame;
+
+        try {
+            frame = vuforiaLocalizer.getFrameQueue().take();
+        }catch (Exception e) {
+            throw new Warning("couldn't find vuforia frame");
+        }
+
+        bitmap = vuforiaLocalizer.convertFrameToBitmap(frame);
+        return bitmap;
+    }
+
+    /**
+     * @return {VuforiaLocalizer} vuforiaLocalizer
+     */
+    public VuforiaLocalizer getVuforiaLocalizer() {
+        return vuforiaLocalizer;
+    }
+}
